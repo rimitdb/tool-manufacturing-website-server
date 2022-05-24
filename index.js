1b0e37c68b117712815259144e64fb34f876e45f
Iqbal <iqbal.du.mis@gmail.com>
2022-05-24 16:28:54 +0600

    get api created

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2omxo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db("ToolManuFac").collection("tools");
+
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools)
+        });
+
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        });
+
+        //Update
+
+        app.put('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const updated = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updatedQuantity = {
+                $set: {
+                    order_quantity: updated.updateQuantity
+                },
+            }
+            const result = await toolCollection.updateOne(filter, updatedQuantity, options);
+            res.send(result);
+        })
+
+        //Delete
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Project-12-server-running')
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
